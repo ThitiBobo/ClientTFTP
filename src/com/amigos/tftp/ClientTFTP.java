f1a966396433da3a37fb6f40c05213ae82a4bcc2
Cécile Besson <cecile.besson@live.fr>
2019-05-16 09:18:35 +0200

    receive

--- src/com/amigos/tftp/ClientTFTP.java
+++ src/com/amigos/tftp/ClientTFTP.java
@@ -7,34 +7,77 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
 
 public class ClientTFTP {
 
     public static int receiveFile(String fileName, int port,InetAddress ia ){
-        try {
-
-        	FileOutputStream fea= new FileOutputStream ("cheminrelatif"+ fileName);
-        	DatagramSocket ds= new DatagramSocket();
-        	TFTPPackage rrq=new TFTPPackage(fileName,""); //mode à changer
-	       	byte[] rrqByte = rrq.getByteArray();
-        	DatagramPacket RRQ= new DatagramPacket(rrqByte,rrqByte.length,ia, port);
-        	ds.send(RRQ);
-
-        	//receive data
-        	//fea.write(data1,2,10);
-        	//fea.close();
-
-
-
-
-		} catch (SocketException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    	while(true)
+    	{
+	        try {
+	
+	        	FileOutputStream fea= new FileOutputStream ("cheminrelatif"+ fileName);
+	        	DatagramSocket ds= new DatagramSocket();
+	        	TFTPPackage rrq= new TFTPPackage(TFTPPackage.OP_CODE_READ,fileName,"ecriture"); //mode à changer
+		       	byte[] rrqByte = rrq.getByteArray();
+	        	DatagramPacket RRQ= new DatagramPacket(rrqByte,rrqByte.length,ia, port);
+	        	ds.send(RRQ);
+	        	
+	        	short numPaquet=1;
+	        	byte[] buffer = new byte[516];
+	    		DatagramPacket dr = new DatagramPacket(buffer, 516);
+	        	do
+	        	{
+	                ds.receive(dr);
+	                TFTPPackage data= new TFTPPackage(dr.getData());
+	                data.getIdBlock();
+	                //ecriture dans le fichier
+	                if(data.getIdBlock()==numPaquet)
+	                {
+		                fea.write(data.getByteArray(),2,516);
+		                sendAcknowledgment(numPaquet,ds,ia,port);
+		                numPaquet++;
+	                }
+	                else
+	                {
+	                	sendAcknowledgment((short) (numPaquet-1),ds,ia,port);
+	                }
+	        	}
+	        
+	        	while(!isLastPacket(dr));
+	        	
+	        	
+	        	ds.setSoTimeout(1000);   // set the timeout in millisecounds.
+	
+	        	while(true) {        // receive data until timeout
+	        		try {
+	        			System.out.println("Receiving message...");
+	        			ds.receive(dr); // receive the packet
+	        			System.out.println("Message received");
+	        			sendAcknowledgment((short) (numPaquet),ds,ia,port);
+	        		}
+	        		catch (SocketTimeoutException e) {
+	        			// timeout exception.
+	        			System.out.println("Timeout reached!!! " + e);
+	        			fea.close();
+	        			ds.close();
+	        		}
+	        	}
+	        
+	     
+	       
+			} catch (SocketException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    }
 
 
 
