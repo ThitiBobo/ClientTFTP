2533e1901afcd3ab78efb6662e76968043e91ac1
Dorian <dorian.naaji@gmail.com>
2019-05-16 08:15:44 +0200

    - separating back and front

--- src/com/amigos/tftp/ClientTFTP.java
+++ src/com/amigos/tftp/ClientTFTP.java
@@ -7,52 +7,60 @@ import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 
-public class ClientTFTP {
+public class ClientTFTP
+{
 
-    public static int receiveFile(){
-        try {
-			DatagramSocket ds= new DatagramSocket();
-		} catch (SocketException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    public static int receiveFile()
+    {
+        try
+        {
+            DatagramSocket ds = new DatagramSocket();
+        }
+        catch (SocketException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
-    public static int sendFile(){
-       
+    public static int sendFile()
+    {
+
     }
 
-    private static boolean isLastPacket(DatagramPacket dp){
-       if(dp.getLength()<516) //vérifier aussi si le paquet n'est pas un code erreur
-       {
-    	   return true;
-       }
-       else
-       {
-    	   return false;
-       }
+    private static boolean isLastPacket(DatagramPacket dp)
+    {
+        if (dp.getLength() < 516) //vï¿½rifier aussi si le paquet n'est pas un code erreur
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
-    private static void sendAcknowledgment(short idBlock, DatagramSocket ds,InetAddress ia, int port){
-       
-		try {
-			 	TFTPPackage ack=new TFTPPackage(idBlock);
-		       	byte[] ackByte = ack.getByteArray();		
-				DatagramPacket dp = new DatagramPacket(ackByte, ackByte.length, ia, port);
-				ds.send(dp);
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-				e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-				e.printStackTrace();
-		}
-		
-        
-       
+    private static void sendAcknowledgment(short idBlock, DatagramSocket ds, InetAddress ia, int port)
+    {
+
+        try
+        {
+            TFTPPackage ack = new TFTPPackage(idBlock);
+            byte[] ackByte = ack.getByteArray();
+            DatagramPacket dp = new DatagramPacket(ackByte, ackByte.length, ia, port);
+            ds.send(dp);
+        }
+        catch (UnknownHostException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
     }
-
-
-
 
 }
