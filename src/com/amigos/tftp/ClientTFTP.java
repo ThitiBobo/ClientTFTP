887942371b659d10ffcb17cf5718a12f1cae935a
Dorian <dorian.naaji@gmail.com>
2019-05-16 09:55:04 +0200

    avancement send

--- src/com/amigos/tftp/ClientTFTP.java
+++ src/com/amigos/tftp/ClientTFTP.java
@@ -1,7 +1,10 @@
 package com.amigos.tftp;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -12,7 +15,7 @@ import java.net.UnknownHostException;
 public class ClientTFTP
 {
 
-    public static int receiveFile(String fileName, int port, InetAddress ia)
+    public static int receiveFile(String fileName, int port, InetAddress ia) throws IOException
     {
         try
         {
@@ -42,28 +45,52 @@ public class ClientTFTP
         return 0;
     }
 
-    public static int sendFile(InetAddress IPserv, int portServ, String nomFichierLocal)
+    public static int sendFile(InetAddress IPserv, short portServ, String pathFichierLocal) throws FileNotFoundException
     {
-
-        TFTPPackage wrq = new TFTPPackage(TFTPPackage.OP_CODE_WRITE, nomFichierLocal, ""); //TO DO modeeee!!!!!!!!!
+        // Ouvrir nomFichierLocal
+        FileInputStream fileStream = new FileInputStream(pathFichierLocal);
+        // Création d'un paquet WRQ
+        TFTPPackage wrq = new TFTPPackage(TFTPPackage.OP_CODE_WRITE, pathFichierLocal, ""); //TO DO modeeee!!!!!!!!!
         byte[] wrqByte = wrq.getByteArray();
-
-        TFTPPackage ack = new TFTPPackage((short) portServ);
 
         try
         {
+            // Affectation port anonyme
             DatagramSocket ds = new DatagramSocket();
+            // Création d'un paquet WRQ
             DatagramPacket dp = new DatagramPacket(wrqByte, wrqByte.length, IPserv, portServ);
             ds.send(dp); //La machine A �met un "WRQ" vers adr_ip_serv, port_serv (Machine B)
 
-            byte[] ackByte = ack.getByteArray();
-            DatagramPacket rep = new DatagramPacket(ackByte, 512);
+            // on reçoit le paquet du serveur après émission du WRQ
+            byte[] ackByte = new byte[4];
+            DatagramPacket rep = new DatagramPacket(ackByte, ackByte.length);
             ds.receive(rep);
+            // Vérifier que la paquet est bien un ACK 0
+            TFTPPackage ack0 = new TFTPPackage((short) 0);
+            // Si oui, on commence l'envoi du fichier
+            if (ack0.getByteArray() == ackByte)
+            {
+                int readResult = 0;
+                int offset = 0;
+                int idBlock = 1;
+                do
+                {
+                    // Bloc de data de 512 octets
+                    byte[] blocData = new byte[512];
+                    // On lit 512 octets dans blocData
+                    readResult = fileStream.read(blocData, offset, blocData.length);
+                    // On crée un nouveau paquet data
 
-            TFTPPackage data = new TFTPPackage((short) 1, ackByte);
-            byte[] dataByte = data.getByteArray();
-            DatagramPacket dpp = new DatagramPacket(dataByte, dataByte.length, IPserv, portServ);
-            ds.send(dpp);
+//                    data = new TFTPPackage((short) 1, ackByte);
+//                    byte[] dataByte = data.getByteArray();
+//                    DatagramPacket dpp = new DatagramPacket(dataByte, dataByte.length, IPserv, portServ);
+//                    ds.send(dpp);
+                    offset += 512;
+                    idBlock++;
+                }
+                // tant que EOF n'a pas été rencontré
+                while (readResult != -1);
+            }
 
         }
         catch (SocketException e)
